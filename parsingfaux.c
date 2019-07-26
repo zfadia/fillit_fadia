008a51ce280fcab1b60052a7865a4f65288bf4d8
zfadia <zfadia@student.42.fr>
2019-07-26 02:21:27 +0200

    fadia2

--- parsingfaux.c
+++ parsingfaux.c
@@ -164,54 +164,6 @@ int				parsing(char *file)
 	return (0);
 }
 
-char	*ft_strtrime(char const *s)
-{
-	size_t	min;
-	size_t	max;
-	size_t	len;
-
-	if (!s)
-		return (NULL);
-	min = 0;
-	while (s[min] != '\0' && (s[min] == '.'))
-		min++;
-	max = ft_strlen(s);
-	while (min < max && (s[max - 1] == '.'))
-		max--;
-	if (min == max)
-		return (ft_strnew(1));
-	len = max - min;
-	return (ft_strsub(s, min, len));
-}
-
-char	**tetrimino(char *file)
-{
-	char	*buf;
-	int		i;
-
-	i = 0;
-	buf = open_stock(file);
-	while (buf[i] == '\n' && buf[i + 1] && buf[i + 1] == '\n')
-	{
-		i++;
-		if (i == 21)
-		{
-			buf[i] = ' ';
-		}
-	}
-	char **res = ft_strsplit(buf, ' ');
-	i = 0;
-	char *tmp;
-	while (res[i])
-	{
-		tmp = ft_strtrime(res[i]);
-		ft_strdel(&res[i]);
-		res[i] = tmp;
-	}
-	ft_strdel(&buf);
-	return (res);
-}
-
 int				main(int argc, char **argv)
 {
 if (argc == 2)
